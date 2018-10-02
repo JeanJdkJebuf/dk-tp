09f43c37e6a961e7bae8585451aac53b13990cd6
JeanJdkJebuf <guillaumecarcema@gmail.com>
2018-10-02 17:04:29 +0200

    création du dossier, des images, des fichiers de départ

---
+++ dklabyrinthe.py
@@ -0,0 +1,15 @@
+######################################################################################
+#           Jeu Donkey Kon Labyrinthe
+#       Jeu dans lequel on doit déplacer DK jusqu'aux bananes à travers le labyrinthe
+######################################################################################
+#           Script Python
+#       Fichiers : dklabyrinthe.py, classes.py, constantes.py, n1, n2 + images
+######################################################################################
+
+
+######################################################################################
+#importation des modules
+######################################################################################
+import constantes
+import classes
+import pygame
