d273be3486d793ea70a6ead1e08844dbbf7fbfb2
JeanJdkJebuf <guillaumecarcema@gmail.com>
2018-10-02 18:39:35 +0200

    L'écran principal marche

--- constantes.py
+++ constantes.py
@@ -1,3 +1,18 @@
 ######################################################################################
 #               constantes pour le jeu dklabyrinthe
 ######################################################################################
+
+######################################################################################
+#               constantes pour la fenêtre principale
+######################################################################################
+#dimensions de la fenêtre principale ( c'est un carré )
+cote_fenetre=450
+#compteur qui ferme le jeu
+continuer_principale=1
+
+#constante représentant l'image principale
+image_icone="accueil.png"
+#nom de la fenetre principale 
+titre_fenetre="Donkey Kong Labyrinthe"
+#positionnement de la fenetre principale
+place_icone=(0,0)
--- dklabyrinthe.py
+++ dklabyrinthe.py
@@ -1,5 +1,5 @@
 ######################################################################################
-#           Jeu Donkey Kon Labyrinthe
+#           Jeu Donkey Kong Labyrinthe
 #       Jeu dans lequel on doit déplacer DK jusqu'aux bananes à travers le labyrinthe
 ######################################################################################
 #           Script Python
@@ -10,6 +10,35 @@
 ######################################################################################
 #importation des modules
 ######################################################################################
-import constantes
+from constantes import *
 import classes
 import pygame
+
+######################################################################################
+#initialisation de pygame
+######################################################################################
+pygame.init()
+
+######################################################################################
+#création de la fenêtre principale
+######################################################################################
+#dimensions de la fenetre principale, le coté est un carré
+fen=pygame.display.set_mode((cote_fenetre,cote_fenetre))
+#ajout de l'image principale
+icone = pygame.image.load(image_icone).convert()
+#on colle l'image principale sur la fenêtre
+fen.blit(icone,place_icone)
+#nom de la fenetre principale
+pygame.display.set_caption(titre_fenetre)
+#raffraichissement d'écran pour afficher l'image principale
+pygame.display.flip()
+
+
+######################################################################################
+#Boucle
+######################################################################################
+#boucle principale
+while continuer_principale :
+    for event in pygame.event.get() :
+        if event.type == pygame.QUIT :
+            continuer_principale=0
